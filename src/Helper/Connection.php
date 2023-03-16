e44a873b5b6d0565a7618f18b03f2ce354f538f4
Alexandr7171 <SashaV020394@mail.ru>
2023-03-16 19:07:15 +0500

    Create PDO connection

--- src/Helper/Connection.php
+++ src/Helper/Connection.php
@@ -2,6 +2,18 @@
 
 namespace App\Helper;
 
-class Connection
+class Connection extends Env
 {
+    /**
+     * create a new PDO connection
+     *
+     * @return \PDO $conn
+     */
+    public function connection()
+    {
+        $dsn = 'mysql:host=' . $this->db_host . ';dbname=' . $this->db_name . ';charset=' . $this->db_charset;
+        $conn = new \PDO($dsn, $this->db_username, $this->db_password);
+        $conn->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        return $conn;
+    }
 }
