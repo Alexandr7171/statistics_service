86c8720c75a96f52dc16b25c3a2b86ee7b320d52
Alexandr7171 <SashaV020394@mail.ru>
2023-03-21 18:47:35 +0500

    New update

---
+++ index.cli.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
+
+$options = getopt('f:');
+
+$json_file_path = $options['f'];
+
+if (!array_key_exists('f', $options)) {
+    throw new Exception('No file specified');
+}
+
+$json_file = file_get_contents($json_file_path);
+$data = json_decode($json_file, true);
